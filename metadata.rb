a4c8abbe7c275eb6c0e43676cc09a03605bbb36a
William Herry <WilliamHerryChina@Gmail.com>
2012-08-22 09:41:32 +0800

    patch for centos

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,10 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Gamewave, Inc."
+maintainer_email "WilliamHerryChina@Gmail.com"
 license          "All rights reserved"
-description      "Installs/Configures openstack-patch"
+description      "Little patch for openstack installation with chef on centos"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+supports "centos"
+
+depends "selinux"
